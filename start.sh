582df20f36fb08870dcebf40a76c8946ede161ea
Szyoo <losiner.y@gmail.com>
2026-01-06 11:48:18 +0900

    更新

--- start.sh
+++ start.sh
@@ -4,10 +4,22 @@ set -eu
 CONFIG_PATH="/etc/headscale/config.yaml"
 APIKEY_PATH="/var/lib/headscale/api-key.txt"
 
+headscale serve --config "$CONFIG_PATH" &
+HS_PID="$!"
+
 if [ ! -f "$APIKEY_PATH" ]; then
-  APIKEY="$(headscale apikeys create --config "$CONFIG_PATH")"
-  echo "HEADSCALE_API_KEY=${APIKEY}"
-  printf "%s" "$APIKEY" > "$APIKEY_PATH"
+  i=0
+  while [ "$i" -lt 30 ]; do
+    if APIKEY="$(headscale apikeys create --config "$CONFIG_PATH" --output json 2>/dev/null | sed -n 's/.*\"apikey\":\"\\([^\"]*\\)\".*/\\1/p')"; then
+      if [ -n "$APIKEY" ]; then
+        echo "HEADSCALE_API_KEY=${APIKEY}"
+        printf "%s" "$APIKEY" > "$APIKEY_PATH"
+        break
+      fi
+    fi
+    i=$((i + 1))
+    sleep 1
+  done
 fi
 
-exec headscale serve --config "$CONFIG_PATH"
+wait "$HS_PID"
